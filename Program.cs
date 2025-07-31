7bd7498e462abb512e8bd211b73d1fd520b3d908
Ricci201 <lucas.ricci@aluno.senai.br>
2025-07-31 10:33:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("A soma é igual a:", soma);
+        Console.WriteLine($"A soma é igual a:", +soma);
+
+        Console.WriteLine("Hello, World!");
+    }
+}
